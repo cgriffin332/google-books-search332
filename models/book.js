3fe76662e0a44ec3d040160a674f493cec7c9b1a
Calvin Griffin <cgriffin332@gmail.com>
2020-11-05 22:36:39 -0500

    hidden api key, created book model

---
+++ models/book.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const BookSchema = new Schema({
+  title: {
+    type: String,
+    trim: true,
+    required: "Title is required"
+  },
+  link: {
+    type: String,
+    required: true
+  },
+  author: {
+      type: Array,
+      required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
+  }
+
+
+});
+
+const Book = mongoose.model("Book", BookSchema);
+
+module.exports = Book;
