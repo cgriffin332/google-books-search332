0b49faacba40c6de8fa25a94ce61dbec0bfe7b6d
Calvin Griffin <cgriffin332@gmail.com>
2020-11-06 01:01:05 -0500

    fix api call and console log properties needed in result object

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,33 @@
 import Saved from "./containers/Saved";
 import Search from "./containers/Search";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { useEffect } from "react";
+import API from "./utils/API";
 import "./App.css";
 
 function App() {
+
+  useEffect(() => {
+    API.getBooks("holes")
+      .then((response) => {
+        console.log(response.data.items);
+        console.log(response.data.items[0].volumeInfo.title);
+        console.log(response.data.items[0].volumeInfo.authors);
+        console.log(response.data.items[0].volumeInfo.imageLinks.smallThumbnail);
+        console.log(response.data.items[0].volumeInfo.description);
+        console.log(response.data.items[0].selfLink);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div className="App">
       <Router>
         <Switch>
+          <Route exact path="/saved" component={Saved} />
           <Route exact path="/" component={Search} />
-          <Route exact path="/saved" compnent={Saved} />
         </Switch>
       </Router>
     </div>
