8564bbb82477665ef94e9a310ecccf10c3918d49
Calvin Griffin <cgriffin332@gmail.com>
2020-11-07 16:33:55 -0500

    updated saved page

--- client/src/containers/Saved.jsx
+++ client/src/containers/Saved.jsx
@@ -1,11 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import SavedCard from "../components/SavedCard";
 
 const Saved = () => {
-    return (
-        <div>
-            <h1>Saved Page</h1>
+  const [allBooks, setAllBooks] = useState([]);
+
+  useEffect(() => {
+    loadBooks();
+  }, []);
+
+  const loadBooks = () => {
+    axios.get("/api/books").then((response) => {
+      // console.log(response.data);
+      setAllBooks(response.data);
+    });
+  };
+
+  return (
+    <div>
+      <h1>Saved Books</h1>
+      <div className="col-3"></div>
+      {allBooks ? (
+        allBooks.map((book) => (
+          <SavedCard
+            title={book.title}
+            authors={book.authors}
+            description={book.description}
+            image={book.imageLinks}
+            googleLink={book.infoLink}
+          />
+        ))
+      ) : (
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <h3>
+                You currently have no saved books. Please search for your
+                favorites and save them.
+              </h3>
+            </div>
+          </div>
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default Saved;
