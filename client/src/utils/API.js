fa3ebc753ebdf3772991d7a274ebb54d36b077a1
Calvin Griffin <cgriffin332@gmail.com>
2020-11-08 12:28:47 -0500

    apikey

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -5,7 +5,7 @@ export default {
   getBooks: function (search) {
     return axios({
       method: "GET",
-      url: `https://www.googleapis.com/books/v1/volumes?q=intitle:${search}&printType=books&key=${API_KEY}`,
+      url: `https://www.googleapis.com/books/v1/volumes?q=intitle:${search}&printType=books&key=${process.env.REACT_APP_API_KEY}`,
     });
   }
 };
