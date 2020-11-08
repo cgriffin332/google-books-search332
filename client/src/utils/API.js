4201fcee1cb1e1c4c9a06975db25707eb1ca8288
Calvin Griffin <cgriffin332@gmail.com>
2020-11-07 22:25:05 -0500

    fixed env file

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,11 +1,11 @@
 import axios from "axios";
-import { default as config } from "./config";
+
 
 export default {
   getBooks: function (search) {
     return axios({
       method: "GET",
-      url: `https://www.googleapis.com/books/v1/volumes?q=intitle:${search}&printType=books&key=${config}`,
+      url: `https://www.googleapis.com/books/v1/volumes?q=intitle:${search}&printType=books&key=${process.env.REACT_APP_API_KEY}`,
     });
   }
 };
